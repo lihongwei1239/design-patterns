0464154040452acd6600a2ed82458e75832dcf7a
lihongwei@lixin360.com <lihongwei@lixin360.com>
2018-03-07 14:20:06 +0800

    dev

---
+++ src/main/java/com/demo/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+
+
+}
